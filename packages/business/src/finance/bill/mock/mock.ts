5942532857338d7f9ae74f80c8349ba5bcf4565c
jorge.machado-Kraken <jorge.vmachado@hotmail.com>
2025-05-07 16:09:18 -0300

    Extend `BILL_MOCK` with additional related mock data
    Enhance the `BILL_MOCK` object by integrating bank, category, finance mocks, and updated type for better consistency and testing coverage. This change aligns with other mock structures and improves modularity in the finance module.

--- packages/business/src/finance/bill/mock/mock.ts
+++ packages/business/src/finance/bill/mock/mock.ts
@@ -1,11 +1,19 @@
 import type { BillEntity } from '../types';
+import { EBillType } from '../../../api';
+
+import { BANK_MOCK } from '../../bank';
+import { BILL_CATEGORY_MOCK } from '../../bill-category';
+import { FINANCE_MOCK } from '../../mock';
 
 export const BILL_MOCK: BillEntity = {
     id: '3f5bf099-4c3b-4ee2-a21f-7d7ccd66d13c',
     year: 2025,
-    type: 'CREDIT_CARD',
+    bank: BANK_MOCK,
+    type: EBillType.CREDIT_CARD,
     name: 'Personal Physical Credit Card Porto',
     total: 2281.31,
+    category: BILL_CATEGORY_MOCK,
+    finance: FINANCE_MOCK,
     name_code: 'personal_physical_credit_card_porto',
     all_paid: false,
     total_paid: 1187.95,
