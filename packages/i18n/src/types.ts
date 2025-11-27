0ce860d3c9b571863dee41665cb819ff4ad91091
jorge.machado-Kraken <jorge.vmachado@hotmail.com>
2025-11-27 14:42:39 -0300

    Add internationalization support with language resources and context provider. Implemented constants for available languages and integrated i18next for translation management. Added initial language JSON files for English and Portuguese.

---
+++ packages/i18n/src/types.ts
@@ -0,0 +1,25 @@
+import {
+    AUTH_SUPPORTED_LANGUAGE_EN,
+    FINANCE_SUPPORTED_LANGUAGE_EN,
+    GEEK_SUPPORTED_LANGUAGE_EN,
+    GLOBAL_SUPPORTED_LANGUAGE_EN,
+    LAW_SUPPORTED_LANGUAGE_EN
+} from './constants';
+
+export type SupportedLang = 'en' | 'pt-BR';
+
+export type TI18nBrands = 'finance' | 'law' | 'geek' | 'auth' | 'default';
+
+export type I18nResources = {
+    [lng: string]: {
+        translation: Record<string, string>;
+    };
+}
+
+export type TResources = {
+    law: Record<SupportedLang, typeof LAW_SUPPORTED_LANGUAGE_EN>;
+    auth: Record<SupportedLang, typeof AUTH_SUPPORTED_LANGUAGE_EN>;
+    geek: Record<SupportedLang, typeof GEEK_SUPPORTED_LANGUAGE_EN>;
+    global: Record<SupportedLang, typeof GLOBAL_SUPPORTED_LANGUAGE_EN>;
+    finance: Record<SupportedLang, typeof FINANCE_SUPPORTED_LANGUAGE_EN>;
+}
---
+++ packages/i18n/src/utils/index.ts
@@ -0,0 +1,1 @@
+export { mergeResources, buildResources } from './utils';
